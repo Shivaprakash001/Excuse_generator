473adafffb98ad4c1560c69b0adc54fce04adc4b
Shivaprakash001 <spchidiri2006@gamil.com>
2025-04-30 23:50:25 +0530

    espeak

--- speak_module.py
+++ speak_module.py
@@ -1,6 +1,8 @@
 import pyttsx3
 
+engine = pyttsx3.init(driverName='sapi5')
 def speak(text):
-    engine = pyttsx3.init()
+    engine.setProperty('rate', 150)  # Speed of speech
     engine.say(text)
     engine.runAndWait()
+
