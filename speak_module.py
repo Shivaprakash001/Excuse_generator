b0d509d34dcf9b1f02c4fc879b8887f73e3de097
Shivaprakash001 <spchidiri2006@gamil.com>
2025-05-04 15:10:39 +0530

    added

--- speak_module.py
+++ speak_module.py
@@ -1,9 +1,17 @@
+import platform
 import pyttsx3
 
+def get_engine():
+    system = platform.system()
+    if system == 'Windows':
+        return pyttsx3.init('sapi5')
+    elif system == 'Darwin':
+        return pyttsx3.init('nsss')  # macOS
+    else:
+        return pyttsx3.init('espeak')  # Linux
+
+engine = get_engine()
 
 def speak(text):
-    engine = pyttsx3.init(driverName='sapi5')
-    engine.setProperty('rate', 250)  # Speed of speech
     engine.say(text)
     engine.runAndWait()
-
